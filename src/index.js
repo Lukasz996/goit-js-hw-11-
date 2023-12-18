070e46a23afa8ed118ebdf38bbbed512bdf598e1
Łukasz <lukaszkogut1996@gmail.com>
2023-12-18 10:10:03 +0100

    update index.js

--- src/index.js
+++ src/index.js
@@ -55,14 +55,34 @@ const lightbox = new SimpleLightbox('.gallery a', {
 });
 
 async function onSearch(event) {
-    event.preventDefault();
-    page = 1;
-    const result = await getPhoto(`${input.value}`, page)
-    loadMoreBtn.style.displey = ' block'
-    if(result.totalHits === 0 ){
-        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-        loadMoreBtn.style.display = 'none'
-    } else {
-        Notiflix.Notify.success(`Hooray! We found ${result.totalHits} images.`);
+  event.preventDefault();
+  page = 1;
+  const result = await getPhoto(`${input.value}`, page);
+  loadMoreBtn.style.displey = ' block';
+  if (result.totalHits === 0) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    loadMoreBtn.style.display = 'none';
+  } else {
+    Notiflix.Notify.success(`Hooray! We found ${result.totalHits} images.`);
+  }
+}
+
+// const markup = createMarkup(result.hits);
+// gallery.innerHTML = createMarkup(result.hits);
+// lightbox.refresh();
+
+function onloadMore() {
+  page += 1;
+  getPhoto(input.value, page).then(restponse => {
+    gallery.insertAdjacentHTML('beforeend', createMarkup(response.hits));
+    lightbox.refresh();
+    if (page * 40 >= response.totalHits) {
+      Notiflix.Notify.warning(
+        "We're sorry, but you've reached the end of search results"
+      );
+      loadMoreBtn.style.display = 'none';
     }
+  });
 }
